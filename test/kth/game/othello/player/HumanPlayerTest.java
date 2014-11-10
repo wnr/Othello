7a78dd8861bd5156c9ad00994ca657a37302db4e
Lucas Wiener <lucas@wiener.se>
2014-11-10 20:38:02 +0100

    Created HumanPlayer class that extends the Player class. Added tests also

---
+++ test/kth/game/othello/player/HumanPlayerTest.java
@@ -0,0 +1,18 @@
+package kth.game.othello.player;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HumanPlayerTest {
+    @Test
+    public void ConstructorTest() {
+        {
+            HumanPlayer player = new HumanPlayer(null, null);
+            Assert.assertEquals(Player.Type.HUMAN, player.getType());
+        }
+        {
+            HumanPlayer player = new HumanPlayer("", "");
+            Assert.assertEquals(Player.Type.HUMAN, player.getType());
+        }
+    }
+}
