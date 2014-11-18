1b3810bbb2dc937705c5c7e985782cb16c4595de
Lucas Wiener <lucas@wiener.se>
2014-11-18 19:28:10 +0100

    Made new interface methods about move strategy implemented in the abstract PlayerImpl class instead of the HumanPlayer and ComputerPlayer

--- src/kth/game/othello/player/PlayerImpl.java
+++ src/kth/game/othello/player/PlayerImpl.java
@@ -1,5 +1,7 @@
 package kth.game.othello.player;
 
+import kth.game.othello.player.movestrategy.MoveStrategy;
+
 /**
  * This is the implementation of the Player interface.
  *
@@ -9,6 +11,7 @@ public abstract class PlayerImpl implements Player {
 	String id;
 	String name;
 	Type type;
+	MoveStrategy moveStrategy;
 
 	/**
 	 * Constructs an othello player structure object.
@@ -16,11 +19,24 @@ public abstract class PlayerImpl implements Player {
 	 * @param id The id is a unique identifier of this player in the context of all players
 	 * @param name The name of the player
 	 * @param type The {@link Type} of the player
+	 * @param moveStrategy The {@link MoveStrategy} of the player
 	 */
-	public PlayerImpl(String id, String name, Type type) {
+	public PlayerImpl(String id, String name, Type type, MoveStrategy moveStrategy) {
 		this.id = id;
 		this.name = name;
 		this.type = type;
+		this.moveStrategy = moveStrategy;
+	}
+
+	/**
+	 * Constructs an othello player structure object, without a move strategy.
+	 *
+	 * @param id The id is a unique identifier of this player in the context of all players
+	 * @param name The name of the player
+	 * @param type The {@link Type} of the player
+	 */
+	public PlayerImpl(String id, String name, Type type) {
+		this(id, name, type, null);
 	}
 
 	@Override
@@ -36,5 +52,15 @@ public abstract class PlayerImpl implements Player {
 	@Override
 	public Type getType() {
 		return type;
+	}
+
+	@Override
+	public MoveStrategy getMoveStrategy() {
+		return moveStrategy;
+	}
+
+	@Override
+	public void setMoveStrategy(MoveStrategy moveStrategy) {
+		this.moveStrategy = moveStrategy;
 	}
 }
