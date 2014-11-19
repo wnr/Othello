a2e98cdb9a9872a7f50c407990fd6d37f39bb913
Lucas Wiener <lucas@wiener.se>
2014-11-19 11:16:45 +0100

    Fixed misspelled

--- src/kth/game/othello/board/OthelloBoardHandlerFactory.java
+++ src/kth/game/othello/board/OthelloBoardHandlerFactory.java
@@ -9,7 +9,7 @@ public class OthelloBoardHandlerFactory {
 	 * Creates an OthelloBoardHandler instance given a Board instance.
 	 *
 	 * @param board The board instance to handle.
-	 * @return An OthelloBoardhandler instance to handle the given board.
+	 * @return An OthelloBoardHandler instance to handle the given board.
 	 */
 	public OthelloBoardHandler createOthelloBoardHandler(Board board) {
 		// TODO: Change this to something more suitable when the new BoardImpl class exists. Then OthelloBoardHandler
