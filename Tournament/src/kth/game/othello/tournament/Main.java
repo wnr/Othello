9e28ae6761fcd7e217b2ebaed3f04a497950b2b4
Lucas Wiener <lucas@wiener.se>
2014-12-06 16:36:38 +0100

    Added tournament module

---
+++ Tournament/src/kth/game/othello/tournament/Main.java
@@ -0,0 +1,17 @@
+package kth.game.othello.tournament;
+
+import kth.game.othello.Othello;
+import kth.game.othello.OthelloFactoryImpl;
+import kth.game.othello.view.swing.OthelloView;
+import kth.game.othello.view.swing.OthelloViewFactory;
+
+/**
+ * Start point for the Tournament Othello game module.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Othello othello = new OthelloFactoryImpl().createComputerGame();
+        OthelloView othelloView = OthelloViewFactory.create(othello, 1000, 5000);
+        othelloView.start();
+    }
+}
