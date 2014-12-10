fa39617d9d1530fec6b5808c27e7b7a09137d53a
Erik Odenman <erik.odenman@gmail.com>
2014-12-10 14:58:28 +0100

    Formatting

--- Tournament/src/kth/game/othello/tournament/Main.java
+++ Tournament/src/kth/game/othello/tournament/Main.java
@@ -1,35 +1,35 @@
 package kth.game.othello.tournament;
 
-import kth.game.othello.player.movestrategy.*;
-
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+
+import kth.game.othello.player.movestrategy.*;
 
 /**
  * Start point for the Tournament Othello game module.
  */
 public class Main {
-    public static void main(String[] args) {
-        // Simple reading of command line arguments.
-        // Should probably be done with a third party tool if we want to extends it
-        boolean showView = false;
-        for (String arg : args) {
-            if (arg.equals("-g") || arg.equals("-gui")) {
-                showView = true;
-            }
-        }
+	public static void main(String[] args) {
+		// Simple reading of command line arguments.
+		// Should probably be done with a third party tool if we want to extends it
+		boolean showView = false;
+		for (String arg : args) {
+			if (arg.equals("-g") || arg.equals("-gui")) {
+				showView = true;
+			}
+		}
 
-        List<MoveStrategy> strategies = new ArrayList<>();
-        strategies.add(new GreedyStrategy());
-        strategies.add(new RandomStrategy());
-        strategies.add(new LowestStrategy());
-        strategies.add(new TopLeftStrategy());
+		List<MoveStrategy> strategies = new ArrayList<>();
+		strategies.add(new GreedyStrategy());
+		strategies.add(new RandomStrategy());
+		strategies.add(new LowestStrategy());
+		strategies.add(new TopLeftStrategy());
 
-        System.out.println("Starting tournament...");
-        Tournament tournament = new Tournament(strategies);
-        List<PlayerResult> results = tournament.play(showView);
-        System.out.println("Tournament done. Results:");
-        new TournamentPrinter(new PrintWriter(System.out)).printResults(results);
-    }
+		System.out.println("Starting tournament...");
+		Tournament tournament = new Tournament(strategies);
+		List<PlayerResult> results = tournament.play(showView);
+		System.out.println("Tournament done. Results:");
+		new TournamentPrinter(new PrintWriter(System.out)).printResults(results);
+	}
 }
--- Tournament/test/kth/game/othello/tournament/PlayerResultTest.java
+++ Tournament/test/kth/game/othello/tournament/PlayerResultTest.java
@@ -5,36 +5,36 @@ import org.junit.Test;
 
 public class PlayerResultTest {
 
-    @Test
-    public void testCompareTo() {
-        {
-            PlayerResult pr1 = new PlayerResult("a", 3, 2, 1);
-            PlayerResult pr2 = new PlayerResult("b", 3, 2, 1);
-            Assert.assertEquals(-1, pr1.compareTo(pr2));
-        }
-        {
-            PlayerResult pr1 = new PlayerResult("a", 3, 2, 1);
-            PlayerResult pr2 = new PlayerResult("b", 3, 2, 2);
-            Assert.assertEquals(1, pr1.compareTo(pr2));
-        }
-        {
-            PlayerResult pr1 = new PlayerResult("a", 2, 0, 6);
-            PlayerResult pr2 = new PlayerResult("b", 3, 5, 0);
-            Assert.assertEquals(1, pr1.compareTo(pr2));
-        }
-    }
+	@Test
+	public void testCompareTo() {
+		{
+			PlayerResult pr1 = new PlayerResult("a", 3, 2, 1);
+			PlayerResult pr2 = new PlayerResult("b", 3, 2, 1);
+			Assert.assertEquals(-1, pr1.compareTo(pr2));
+		}
+		{
+			PlayerResult pr1 = new PlayerResult("a", 3, 2, 1);
+			PlayerResult pr2 = new PlayerResult("b", 3, 2, 2);
+			Assert.assertEquals(1, pr1.compareTo(pr2));
+		}
+		{
+			PlayerResult pr1 = new PlayerResult("a", 2, 0, 6);
+			PlayerResult pr2 = new PlayerResult("b", 3, 5, 0);
+			Assert.assertEquals(1, pr1.compareTo(pr2));
+		}
+	}
 
-    @Test
-    public void testIsScoreEqual() {
-        {
-            PlayerResult pr1 = new PlayerResult("a", 3, 2, 1);
-            PlayerResult pr2 = new PlayerResult("b", 3, 2, 1);
-            Assert.assertEquals(true, pr1.isScoreEqual(pr2));
-        }
-        {
-            PlayerResult pr1 = new PlayerResult("a", 3, 2, 1);
-            PlayerResult pr2 = new PlayerResult("b", 3, 2, 2);
-            Assert.assertEquals(false, pr1.isScoreEqual(pr2));
-        }
-    }
+	@Test
+	public void testIsScoreEqual() {
+		{
+			PlayerResult pr1 = new PlayerResult("a", 3, 2, 1);
+			PlayerResult pr2 = new PlayerResult("b", 3, 2, 1);
+			Assert.assertEquals(true, pr1.isScoreEqual(pr2));
+		}
+		{
+			PlayerResult pr1 = new PlayerResult("a", 3, 2, 1);
+			PlayerResult pr2 = new PlayerResult("b", 3, 2, 2);
+			Assert.assertEquals(false, pr1.isScoreEqual(pr2));
+		}
+	}
 }
